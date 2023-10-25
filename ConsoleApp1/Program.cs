eae81d2504a74c34cb93528a1b0bc5160ffce96d
valekpot92 <148702643+valekpot92@users.noreply.github.com>
2023-10-26 00:24:08 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.WriteLine("Число с точностью до сотых");
+Console.WriteLine("1.02");
+Console.ReadKey();
+Console.WriteLine("Задание номер 2");
+Console.ReadKey();
+Console.WriteLine("Выведите на экран число пользователя");
+Console.Write("Вы ввели число: ");
+var namber = Console.ReadLine();
+Console.ReadKey();
+Console.WriteLine("Задание 3");
+Console.ReadKey();
+Console.WriteLine("Должно появиться, вот такое число вы ввели.");
+Console.Write("Введите число: ");
+var namber_1 = Console.ReadLine();
+Console.WriteLine($"Вы ввели число {namber_1}");
+Console.ReadKey();
+
+Console.WriteLine("Задание 4: ");
+Console.WriteLine("Вывести на одной строке числа 1, 13 и 49 с одним пробелом");
+Console.ReadKey();
+Console.WriteLine("1 13 49");
+Console.WriteLine("Вывести на одной строке числа 7 15 100 с двумя пробелами между");
+Console.WriteLine("7  15  100");
+Console.ReadLine();
+Console.WriteLine("пропущу задания 1.7 и 1.8 в них нет смысла");
+Console.ReadKey();
+Console.WriteLine("Вывести числа 50 и 10 одно под другим");
+Console.ReadKey();
+Console.WriteLine("50\n10");
+Console.WriteLine("Вывести числа 5 10 и 21 одно под другим");
+Console.ReadKey();
+Console.WriteLine("5\n10\n21");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadKey();
